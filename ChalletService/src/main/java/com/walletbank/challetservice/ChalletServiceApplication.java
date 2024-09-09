1539da14404e972b64fe91dfc48443ccf3263fbe
최태민 <wxogud3377@naver.com>
2024-09-09 16:30:45 +0900

    init setting

---
+++ ChalletService/src/main/java/com/walletbank/challetservice/ChalletServiceApplication.java
@@ -0,0 +1,13 @@
+package com.walletbank.challetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChalletServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChalletServiceApplication.class, args);
+    }
+
+}
---
+++ ChalletService/src/test/java/com/walletbank/challetservice/ChalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walletbank.challetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChalletServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
