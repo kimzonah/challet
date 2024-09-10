b0f1318327c8736c609e5e30e8e8d3f93aac5745
kkkhhh4930 <kkkhhh4930@naver.com>
2024-09-10 10:58:41 +0900

    feat: PWA 설정 추가 (아이콘 및 manifest.json 설정)

---
+++ FrontEnd/challet/vite.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate", // 자동 업데이트 설정
+      includeAssets: [
+        "favicon.svg",
+        "robots.txt",
+        "apple-touch-icon.png",
+        "assets/icons/*",
+      ], // 캐싱할 자산 목록
+      manifest: {
+        name: "Challet",
+        short_name: "Challet",
+        description: "Your Challet Application",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icons/apple-touch-icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "icons/favicon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+      },
+    }),
+  ],
+});
