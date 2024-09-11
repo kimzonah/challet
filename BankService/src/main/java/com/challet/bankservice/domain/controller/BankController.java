2c25ab53df86efc0340ee204267c0f0d65fab685
최태민 <wxogud3377@naver.com>
2024-09-12 00:29:40 +0900

    feat: swagger 계좌이체 및 요청DTO

--- BankService/src/main/java/com/challet/bankservice/domain/controller/BankController.java
+++ BankService/src/main/java/com/challet/bankservice/domain/controller/BankController.java
@@ -1,5 +1,6 @@
 package com.challet.bankservice.domain.controller;
 
+import com.challet.bankservice.domain.dto.request.AccountTransferRequestDTO;
 import com.challet.bankservice.domain.dto.request.PaymentRequestDTO;
 import com.challet.bankservice.domain.dto.response.AccountInfoResponseDTO;
 import com.challet.bankservice.domain.dto.response.TransactionDetailResponseDto;
@@ -64,13 +65,23 @@ public class BankController {
         return null;
     }
 
-    @PostMapping("/payments")
+    @PostMapping("/challet-banks/payments")
     @Operation(summary = "결제 서비스", description = "결제 금액, 결제 장소, 결제 카테고리 데이터를 이용한 결제")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "결제 성공"),
             @ApiResponse(responseCode = "400", description = "결제 실패", content = @Content(schema = @Schema(implementation = Exception.class))),
     })
     public ResponseEntity processPayment(@RequestBody PaymentRequestDTO paymentRequestDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/challet-banks/account-transfers")
+    @Operation(summary = "계좌 이체 서비스", description = "이체 계좌, 이체 금액  데이터를 이용한 결제")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "이체 성공"),
+            @ApiResponse(responseCode = "400", description = "이체 실패", content = @Content(schema = @Schema(implementation = Exception.class))),
+    })
+    public ResponseEntity processAccountTransfer(@RequestBody AccountTransferRequestDTO accountTransferRequestDTO){
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
