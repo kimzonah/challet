61107a3dcc49be59fc5a87d34b34f0540840e4d6
YuJin Son <156387293+yjjj1612@users.noreply.github.com>
2024-09-11 16:32:33 +0900

    feat: 지갑페이지 구현

--- FrontEnd/challet/src/App.tsx
+++ FrontEnd/challet/src/App.tsx
@@ -1,15 +1,28 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navigation';
+import WalletPage from './pages/WalletPage';
+import ChallengePage from './pages/ChallengePage';
+import AnalysisPage from './pages/AnalysisPage';
+import MyPage from './pages/MyPage';
 import LoginPage from './pages/LoginPage';
 import './App.css';
-import ChallengePage from './pages/ChallengePage';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path='/challet-service/users/login' element={<LoginPage />} />
-        <Route path='/challenge' element={<ChallengePage />} />
-      </Routes>
+      <div className='min-h-screen flex flex-col justify-between'>
+        {/* Routes 설정 */}
+        <Routes>
+          <Route path='/challet-service/users/login' element={<LoginPage />} />
+          <Route path='/wallet' element={<WalletPage />} />
+          <Route path='/challenge' element={<ChallengePage />} />
+          <Route path='/analysis' element={<AnalysisPage />} />
+          <Route path='/my' element={<MyPage />} />
+        </Routes>
+        {/* 네비게이션 바 */}
+        <Navbar />
+      </div>
     </Router>
   );
 }
