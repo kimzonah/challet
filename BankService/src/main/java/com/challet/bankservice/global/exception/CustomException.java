f9d19e7ba62f32b0aea93614ef1db98a39f2e8b9
최태민 <wxogud3377@naver.com>
2024-09-17 21:03:06 +0900

    add: 계좌 생성 실패시 예외

--- BankService/src/main/java/com/challet/bankservice/global/exception/CustomException.java
+++ BankService/src/main/java/com/challet/bankservice/global/exception/CustomException.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum CustomException {
-    NOT_FOUND_USER_EXCEPTION(400,"NotFoundUserException","유저가 존재하지 않습니다.");
+    NOT_FOUND_USER_EXCEPTION(400,"NotFoundUserException","유저가 존재하지 않습니다."),
+    NOT_CREATE_USER_ACCOUNT_EXCEPTION(400, "NotCreateUserAccountException", "계좌 생성 실패");
 
     private int statusNum;
     private String errorCode;
