78b0a9fddde1a22c6c0786e3590dae0ed71905cf
YuJin Son <156387293+yjjj1612@users.noreply.github.com>
2024-09-19 17:53:53 +0900

    feat: 송금페이지 설계

--- FrontEnd/challet/dev-dist/sw.js
+++ FrontEnd/challet/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qgija1ddtt8"
+    "revision": "0.pic65r98g4o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
