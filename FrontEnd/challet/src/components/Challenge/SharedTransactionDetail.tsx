a4c947860bf21ccdb4a73f69618c289a7ac51ba0
하정수 <kmk2528@naver.com>
2024-09-19 14:45:12 +0900

    feat: 공유거래상세페이지 스크롤바 없앰

--- FrontEnd/challet/src/components/Challenge/SharedTransactionDetail.tsx
+++ FrontEnd/challet/src/components/Challenge/SharedTransactionDetail.tsx
@@ -29,7 +29,7 @@ const SharedTransactionDetail = () => {
   }
 
   return (
-    <div className='p-4'>
+    <div className='p-4 '>
       {/* 탑바 */}
       <div className='fixed top-0 left-0 right-0 bg-white z-10'>
         <div className='flex justify-between pt-8 px-3 items-center'>
@@ -50,7 +50,7 @@ const SharedTransactionDetail = () => {
         </div>
       </div>
 
-      <div className='mt-16'>
+      <div className='mt-16 scrollbar-hide overflow-y-auto max-h-[580px]'>
         <div className='flex items-center'>
           <img
             src={transactionDetail.profileImage || '/default_profile.png'}
@@ -73,7 +73,7 @@ const SharedTransactionDetail = () => {
         <p className='mb-4 text-lg'>{transactionDetail.content}</p>
 
         {/* 이모티콘 및 댓글 개수 */}
-        <div className='flex space-x-4'>
+        <div className='flex space-x-4 py-2 border-b-2 border-dashed'>
           <div className='flex items-center'>
             <span className='text-purple-500 mr-1'>😍</span>
             <span>{transactionDetail.threeEmojiNum}</span>
@@ -99,7 +99,7 @@ const SharedTransactionDetail = () => {
       </div>
 
       {/* 댓글 입력창 */}
-      <div className='fixed bottom-0 left-0 right-0 bg-white p-4'>
+      <div className='fixed bottom-0 left-0 right-0 bg-white p-4 border-t-2'>
         <div className='flex items-center'>
           <input
             type='text'
