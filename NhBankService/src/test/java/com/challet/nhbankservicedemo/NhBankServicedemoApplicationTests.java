10467218ff5573f0de58f3abfaafaa568319ec72
최태민 <wxogud3377@naver.com>
2024-09-20 03:34:59 +0900

    feat: QueryDsl 설정

--- NhBankService/src/test/java/com/challet/nhbankservicedemo/NhBankServicedemoApplicationTests.java
+++ NhBankService/src/test/java/com/challet/nhbankservicedemo/NhBankServicedemoApplicationTests.java
@@ -1,9 +1,7 @@
 package com.challet.nhbankservicedemo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class NhBankServicedemoApplicationTests {
 
     @Test
