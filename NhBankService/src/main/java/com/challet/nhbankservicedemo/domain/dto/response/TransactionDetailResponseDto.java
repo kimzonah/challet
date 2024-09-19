788a6c0789ae38ab3c832bd47bf529efe72365bf
최태민 <wxogud3377@naver.com>
2024-09-20 03:38:40 +0900

    feat: record 적용

--- NhBankService/src/main/java/com/challet/nhbankservicedemo/domain/dto/response/TransactionDetailResponseDto.java
+++ NhBankService/src/main/java/com/challet/nhbankservicedemo/domain/dto/response/TransactionDetailResponseDto.java
@@ -1,27 +1,28 @@
 package com.challet.nhbankservicedemo.domain.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Schema(description = "거래 상세 조회 응답 DTO")
-public class TransactionDetailResponseDto {
+public record TransactionDetailResponseDTO(
+
     @Schema(description = "거래 금액")
-    private Long transactionAmount;
+    Long transactionAmount,
+
     @Schema(description = "거래 일시")
-    private LocalDateTime transactionDatetime;
+    LocalDateTime transactionDatetime,
+
     @Schema(description = "입금처")
-    private String deposit;
+    String deposit,
+
     @Schema(description = "출금처")
-    private String withdrawal;
+    String withdrawal,
+
     @Schema(description = "거래 후 잔액")
-    private Long transactionBalance;
-    @Schema(description = "카테고리", allowableValues = {"DELIVERY","TRANSPORT","COFFEE","SHOPPING"})
-    private String category;
+    Long transactionBalance,
+
+    @Schema(description = "카테고리", allowableValues = {"DELIVERY", "TRANSPORT", "COFFEE", "SHOPPING"})
+    String category
+) {
+
 }
