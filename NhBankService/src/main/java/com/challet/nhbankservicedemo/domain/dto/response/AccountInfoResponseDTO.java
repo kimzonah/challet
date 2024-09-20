c95d59239b8b50f792ce8d6cfffacccae50e61be
최태민 <wxogud3377@naver.com>
2024-09-20 16:45:18 +0900

    refactor: 은행 오타 수정

--- NhBankService/src/main/java/com/challet/nhbankservicedemo/domain/dto/response/AccountInfoResponseDTO.java
+++ NhBankService/src/main/java/com/challet/nhbankservicedemo/domain/dto/response/AccountInfoResponseDTO.java
@@ -2,13 +2,13 @@ package com.challet.nhbankservicedemo.domain.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "챌렛은행 조회 응답 DTO")
+@Schema(description = "농협은행 조회 응답 DTO")
 public record AccountInfoResponseDTO(
 
-    @Schema(description = "챌렛은행 계좌 ID")
+    @Schema(description = "농협은행 계좌 ID")
     Long id,
 
-    @Schema(description = "챌렛은행 계좌 번호")
+    @Schema(description = "농협은행 계좌 번호")
     String accountNumber,
 
     @Schema(description = "잔액")
