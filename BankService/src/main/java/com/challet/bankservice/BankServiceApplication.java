87e6d30afdd9f46ea64c390ba954bcb3cc8b566c
최태민 <wxogud3377@naver.com>
2024-09-24 01:26:50 +0900

    feat: MSA 서비스간의 Feign Client 통신 방법 설정

--- BankService/src/main/java/com/challet/bankservice/BankServiceApplication.java
+++ BankService/src/main/java/com/challet/bankservice/BankServiceApplication.java
@@ -2,8 +2,10 @@ package com.challet.bankservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BankServiceApplication {
 
     public static void main(String[] args) {
