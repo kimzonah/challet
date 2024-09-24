3f76f685395f5f167deb7c659c06b9be8dd18eff
김선하 <ksh219805@gmail.com>
2024-09-24 10:29:18 +0900

    fix: requestDTO 유효성 검사 수정, category 타입 수정

--- ChalletService/src/main/java/com/challet/challetservice/domain/dto/request/ChallengeRegisterRequestDTO.java
+++ ChalletService/src/main/java/com/challet/challetservice/domain/dto/request/ChallengeRegisterRequestDTO.java
@@ -1,5 +1,6 @@
 package com.challet.challetservice.domain.dto.request;
 
+import com.challet.challetservice.domain.entity.Category;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -13,33 +14,33 @@ public record ChallengeRegisterRequestDTO(
 
     @Schema(description = "챌린지 카테고리", allowableValues = {"DELIVERY", "TRANSPORT", "COFFEE",
         "SHOPPING"})
-    @NotBlank(message = "챌린지 카테고리는 필수 입력값 입니다.")
-    String category,
+    @NotNull(message = "카테고리는 필수 항목입니다.")
+    Category category,
 
     @Schema(description = "챌린지 제목")
-    @NotBlank(message = "챌린지 제목은 필수 입력값 입니다.")
+    @NotBlank(message = "제목은 필수 항목입니다.")
     String title,
 
     @Schema(description = "챌린지 소비한도")
-    @NotBlank(message = "소비한도는 필수 입력값 입니다.")
+    @NotNull(message = "소비한도는 필수 항목입니다.")
     Long spendingLimit,
 
     @Schema(description = "챌린지 시작날짜")
-    @NotBlank(message = "챌린지 시작날짜는 필수 입력값 입니다.")
+    @NotNull(message = "시작 날짜는 필수 항목입니다.")
     LocalDate startDate,
 
     @Schema(description = "챌린지 마감날짜")
-    @NotBlank(message = "챌린지 마감날짜는 필수 입력값 입니다.")
+    @NotNull(message = "마감 날짜는 필수 항목입니다.")
     LocalDate endDate,
 
     @Schema(description = "참여 가능 인원")
-    @NotBlank(message = "참여 인원은 필수 입력값 입니다.")
+    @NotNull(message = "참여 가능 인원은 필수 항목입니다.")
     @Min(value = 1, message = "1명이상이어야 합니다.")
     @Max(value = 10, message = "10명 이하여야 합니다.")
     Integer maxParticipants,
 
     @Schema(description = "공개 여부 (비공개 : false, 공개 : true)")
-    @NotBlank(message = "공개 여부는 필수 입력값 입니다.")
+    @NotNull(message = "공개 여부는 필수 항목입니다.")
     Boolean isPublic
 
 ) {
