4627cd6a287f2ba8d23f810c01088f3c14496710
YuJin Son <156387293+yjjj1612@users.noreply.github.com>
2024-09-24 17:02:44 +0900

    merge

--- FrontEnd/challet/dev-dist/sw.js
+++ FrontEnd/challet/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gukabpooufg"
+    "revision": "0.u2hao8iov3o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
