267c869f4220ccb9064176e16860bc2bb297f58c
YuJin Son <156387293+yjjj1612@users.noreply.github.com>
2024-09-25 13:51:48 +0900

    내역페이지 컴포넌트 분리

--- FrontEnd/challet/dev-dist/sw.js
+++ FrontEnd/challet/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gqk26jdak2g"
+    "revision": "0.vhmds5g7oq8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
