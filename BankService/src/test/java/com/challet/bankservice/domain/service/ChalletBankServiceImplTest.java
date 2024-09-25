03fab3094f0ef005146014880f5b58fe1b6fd47b
최태민 <wxogud3377@naver.com>
2024-09-25 10:27:01 +0900

    refactor: 테스트 코드 수정(DB전체 삭제에서 test 데이터만 삭제)

--- BankService/src/test/java/com/challet/bankservice/domain/service/ChalletBankServiceImplTest.java
+++ BankService/src/test/java/com/challet/bankservice/domain/service/ChalletBankServiceImplTest.java
@@ -43,7 +43,7 @@ class ChalletBankServiceImplTest {
 
     @AfterEach
     public void after() {
-        challetBankRepository.deleteAll();
+        challetBankRepository.delete(testAccount);
     }
 
 
