e831ab77155e18393b62bfaa58862524d43eb04e
최태민 <wxogud3377@naver.com>
2024-09-26 17:49:32 +0900

    refactor: 코드리뷰 수정

--- ChalletService/src/main/java/com/challet/challetservice/domain/controller/ChallengeMessageController.java
+++ ChalletService/src/main/java/com/challet/challetservice/domain/controller/ChallengeMessageController.java
@@ -3,6 +3,7 @@ package com.challet.challetservice.domain.controller;
 import com.challet.challetservice.domain.request.PaymentHttpMessageRequestDTO;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -12,13 +13,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/challet/message")
 @Tag(name = "ChallengeMessageController", description = "ch-bank에서 결제 시 메시지 받는 controller")
 public class ChallengeMessageController {
 
-    private static final Logger log = LoggerFactory.getLogger(ChallengeMessageController.class);
     private final SimpMessagingTemplate messagingTemplate;
 
     @PostMapping("/payments")
