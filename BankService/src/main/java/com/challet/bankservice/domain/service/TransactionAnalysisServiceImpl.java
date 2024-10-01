ad0abd45fa4b14518fcb52334e55963bcdb54c66
최태민 <wxogud3377@naver.com>
2024-10-02 01:09:21 +0900

    feat: challet 서비스에 분석에 필요한 user 정보와 비슷한 회원정보요청

--- BankService/src/main/java/com/challet/bankservice/domain/service/TransactionAnalysisServiceImpl.java
+++ BankService/src/main/java/com/challet/bankservice/domain/service/TransactionAnalysisServiceImpl.java
@@ -1,11 +1,13 @@
 package com.challet.bankservice.domain.service;
 
 import com.challet.bankservice.domain.dto.request.MonthlyTransactionRequestDTO;
+import com.challet.bankservice.domain.dto.request.UserInfoMessageRequestDTO;
 import com.challet.bankservice.domain.dto.response.CategoryAmountResponseDTO;
 import com.challet.bankservice.domain.dto.response.CategoryAmountResponseListDTO;
 import com.challet.bankservice.domain.dto.response.MonthlyTransactionHistoryDTO;
 import com.challet.bankservice.domain.dto.response.MonthlyTransactionHistoryListDTO;
 import com.challet.bankservice.domain.repository.ChalletBankRepository;
+import com.challet.bankservice.global.client.ChalletFeignClient;
 import com.challet.bankservice.global.client.KbBankFeignClient;
 import com.challet.bankservice.global.client.NhBankFeignClient;
 import com.challet.bankservice.global.client.ShBankFeignClient;
@@ -25,6 +27,7 @@ public class TransactionAnalysisServiceImpl implements TransactionAnalysisServic
     private final KbBankFeignClient kbBankFeignClient;
     private final NhBankFeignClient nhBankFeignClient;
     private final ShBankFeignClient shBankFeignClient;
+    private final ChalletFeignClient challetFeignClient;
 
 
     @Override
@@ -66,6 +69,8 @@ public class TransactionAnalysisServiceImpl implements TransactionAnalysisServic
             마이데이터에 연결된 하나의 전화번호이기 때문에 여러 사용자일 경우의 로직 수정이 필요
             user 테이블에서 전화번호 리스트도 필요        
          */
+        UserInfoMessageRequestDTO userInfo = challetFeignClient.getUserInfo(tokenHeader);
+
         String phoneNumber = jwtUtil.getLoginUserPhoneNumber(tokenHeader);
 
         CategoryAmountResponseListDTO chBankCategory = challetBankRepository.getTransactionByGroupCategory(
