5f7e78bafa243bfa4585e6a5a95a4add1a45bc12
박창호 <pch5521@gmail.com>
2024-10-02 14:29:56 +0900

    feat: Status, Category, Title로 챌린지를 검색하는 메서드 추가

--- ChalletService/src/main/java/com/challet/challetservice/domain/elasticsearch/repository/SearchedChallengeRepository.java
+++ ChalletService/src/main/java/com/challet/challetservice/domain/elasticsearch/repository/SearchedChallengeRepository.java
@@ -2,7 +2,10 @@ package com.challet.challetservice.domain.elasticsearch.repository;
 
 import com.challet.challetservice.domain.entity.SearchedChallenge;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import java.util.List;
 
 public interface SearchedChallengeRepository extends ElasticsearchRepository<SearchedChallenge, Long> {
+
+    List<SearchedChallenge> findByStatusAndCategoryAndTitleContaining(String status, String category, String title);
 
 }
