7ac29ee7160fbac7b973d296de3b8b01a43aff62
HaJeongSu <htg770@gmail.com>
2024-10-04 01:45:26 +0900

    feat: 새로운 거래내역 발생 시 버튼 표시

--- FrontEnd/challet/dev-dist/sw.js
+++ FrontEnd/challet/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fmh0sqm81no"
+    "revision": "0.7kvpmju2ki8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
