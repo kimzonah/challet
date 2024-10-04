46d7af86e7e60ddcbc35fc8c627039ebd788be0e
prao <pch5521@gmail.com>
2024-10-04 20:35:14 +0900

    feat: 검색 Repository 패키지 변경

--- ShBankService/src/main/java/com/challet/shbankservice/domain/elasticsearch/repository/SearchedTransactionRepository.java
+++ ShBankService/src/main/java/com/challet/shbankservice/domain/elasticsearch/repository/SearchedTransactionRepository.java
@@ -1,4 +1,4 @@
-package com.challet.shbankservice.domain.repository;
+package com.challet.shbankservice.domain.elasticsearch.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
