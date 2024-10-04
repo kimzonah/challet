917593a13e878e38baf80f14379e81c0dd956b57
prao <pch5521@gmail.com>
2024-10-04 21:22:33 +0900

    feat: 예외 추가

--- NhBankService/src/main/java/com/challet/nhbankservicedemo/global/exception/CustomException.java
+++ NhBankService/src/main/java/com/challet/nhbankservicedemo/global/exception/CustomException.java
@@ -11,6 +11,9 @@ public enum CustomException {
     NOT_GET_TRANSACTION_DETAIL_EXCEPTION(400, "NotGetTransactionDetailException",
         "잘못된 거래내역 입니다.(동일한 거래내역 2개이상)"),
 
+    NOT_ENOUGH_FUNDS_EXCEPTION(400, "NotEnoughFundsException", "잔액이 부족합니다"),
+
+
     ACCOUNT_NOT_FOUND_EXCEPTION(404, "AccountNotFoundException", "입력한 계좌가 존재하지 않습니다");
 
     private int statusNum;
