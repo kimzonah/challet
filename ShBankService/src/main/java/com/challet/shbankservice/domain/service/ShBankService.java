7c6cd0f5efd4a8b8723f2c2db583cb912ceab0ee
prao <pch5521@gmail.com>
2024-10-04 20:27:58 +0900

    feat: 결제, 검색 기능 추가

--- ShBankService/src/main/java/com/challet/shbankservice/domain/service/ShBankService.java
+++ ShBankService/src/main/java/com/challet/shbankservice/domain/service/ShBankService.java
@@ -1,16 +1,20 @@
 package com.challet.shbankservice.domain.service;
 
+import java.util.Map;
+
 import com.challet.shbankservice.domain.dto.request.AccountTransferRequestDTO;
 import com.challet.shbankservice.domain.dto.request.BankToAnalysisMessageRequestDTO;
 import com.challet.shbankservice.domain.dto.request.MonthlyTransactionRequestDTO;
+import com.challet.shbankservice.domain.dto.request.PaymentRequestDTO;
+import com.challet.shbankservice.domain.dto.request.SearchTransactionRequestDTO;
 import com.challet.shbankservice.domain.dto.response.AccountInfoResponseListDTO;
 import com.challet.shbankservice.domain.dto.response.BankTransferResponseDTO;
-import com.challet.shbankservice.domain.dto.response.CategoryAmountResponseListDTO;
 import com.challet.shbankservice.domain.dto.response.MonthlyTransactionHistoryListDTO;
+import com.challet.shbankservice.domain.dto.response.PaymentResponseDTO;
+import com.challet.shbankservice.domain.dto.response.SearchedTransactionResponseDTO;
 import com.challet.shbankservice.domain.dto.response.TransactionDetailResponseDTO;
 import com.challet.shbankservice.domain.dto.response.TransactionResponseListDTO;
 import com.challet.shbankservice.domain.entity.Category;
-import java.util.Map;
 
 public interface ShBankService {
 
@@ -28,4 +32,9 @@ public interface ShBankService {
         MonthlyTransactionRequestDTO requestDTO);
 
     Map<Category, Long> getTransactionByGroupCategory(BankToAnalysisMessageRequestDTO requestDTO);
+
+    SearchedTransactionResponseDTO searchTransaction(
+        SearchTransactionRequestDTO searchTransactionRequestDTO);
+
+    PaymentResponseDTO qrPayment(Long accountId, PaymentRequestDTO paymentRequestDTO);
 }
--- ShBankService/src/main/java/com/challet/shbankservice/domain/service/ShBankServiceImpl.java
+++ ShBankService/src/main/java/com/challet/shbankservice/domain/service/ShBankServiceImpl.java
@@ -1,105 +1,186 @@
 package com.challet.shbankservice.domain.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.hibernate.NonUniqueResultException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
 import com.challet.shbankservice.domain.dto.request.AccountTransferRequestDTO;
 import com.challet.shbankservice.domain.dto.request.BankToAnalysisMessageRequestDTO;
 import com.challet.shbankservice.domain.dto.request.MonthlyTransactionRequestDTO;
+import com.challet.shbankservice.domain.dto.request.PaymentRequestDTO;
+import com.challet.shbankservice.domain.dto.request.SearchTransactionRequestDTO;
 import com.challet.shbankservice.domain.dto.response.AccountInfoResponseListDTO;
 import com.challet.shbankservice.domain.dto.response.BankTransferResponseDTO;
-import com.challet.shbankservice.domain.dto.response.CategoryAmountResponseListDTO;
 import com.challet.shbankservice.domain.dto.response.MonthlyTransactionHistoryListDTO;
+import com.challet.shbankservice.domain.dto.response.PaymentResponseDTO;
+import com.challet.shbankservice.domain.dto.response.SearchedTransactionResponseDTO;
 import com.challet.shbankservice.domain.dto.response.TransactionDetailResponseDTO;
 import com.challet.shbankservice.domain.dto.response.TransactionResponseDTO;
 import com.challet.shbankservice.domain.dto.response.TransactionResponseListDTO;
 import com.challet.shbankservice.domain.entity.Category;
+import com.challet.shbankservice.domain.entity.SearchedTransaction;
 import com.challet.shbankservice.domain.entity.ShBank;
 import com.challet.shbankservice.domain.entity.ShBankTransaction;
+import com.challet.shbankservice.domain.repository.SearchedTransactionRepository;
 import com.challet.shbankservice.domain.repository.ShBankRepository;
+import com.challet.shbankservice.domain.repository.ShBankTransactionRepository;
 import com.challet.shbankservice.global.exception.CustomException;
 import com.challet.shbankservice.global.exception.ExceptionResponse;
 import com.challet.shbankservice.global.util.JwtUtil;
+
 import jakarta.transaction.Transactional;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.NonUniqueResultException;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ShBankServiceImpl implements ShBankService {
 
-    private final ShBankRepository shBankRepository;
-    private final JwtUtil jwtUtil;
+	private final ShBankRepository shBankRepository;
+	private final ShBankTransactionRepository shBankTransactionRepository;
+	private final SearchedTransactionRepository searchedTransactionRepository;
+	private final JwtUtil jwtUtil;
 
-    @Override
-    public AccountInfoResponseListDTO getAccountsByPhoneNumber(String tokenHeader) {
-        String loginUserPhoneNumber = jwtUtil.getLoginUserPhoneNumber(tokenHeader);
-        return shBankRepository.getAccountInfoByPhoneNumber(
-            loginUserPhoneNumber);
-    }
+	@Override
+	public AccountInfoResponseListDTO getAccountsByPhoneNumber(String tokenHeader) {
+		String loginUserPhoneNumber = jwtUtil.getLoginUserPhoneNumber(tokenHeader);
+		return shBankRepository.getAccountInfoByPhoneNumber(
+			loginUserPhoneNumber);
+	}
 
-    @Transactional
-    @Override
-    public TransactionResponseListDTO getAccountTransactionList(Long accountId) {
-        Long accountBalance = shBankRepository.getAccountBalanceById(accountId);
-        List<TransactionResponseDTO> transactionList = shBankRepository.getTransactionByAccountId(
-            accountId);
+	@Transactional
+	@Override
+	public TransactionResponseListDTO getAccountTransactionList(Long accountId) {
+		Long accountBalance = shBankRepository.getAccountBalanceById(accountId);
+		List<TransactionResponseDTO> transactionList = shBankRepository.getTransactionByAccountId(
+			accountId);
 
-        return TransactionResponseListDTO
-            .builder()
-            .transactionCount((long) transactionList.size())
-            .accountBalance(accountBalance)
-            .transactionResponseDTO(transactionList).build();
-    }
+		return TransactionResponseListDTO
+			.builder()
+			.transactionCount((long)transactionList.size())
+			.accountBalance(accountBalance)
+			.transactionResponseDTO(transactionList).build();
+	}
 
-    @Override
-    public TransactionDetailResponseDTO getTransactionInfo(Long transactionId) {
-        try {
-            return Optional.ofNullable(
-                    shBankRepository.getTransactionDetailById(transactionId))
-                .orElseThrow(() -> new ExceptionResponse(
-                    CustomException.NOT_FOUND_TRANSACTION_DETAIL_EXCEPTION));
-        } catch (NonUniqueResultException e) {
-            throw new ExceptionResponse(CustomException.NOT_GET_TRANSACTION_DETAIL_EXCEPTION);
-        }
-    }
+	@Override
+	public TransactionDetailResponseDTO getTransactionInfo(Long transactionId) {
+		try {
+			return Optional.ofNullable(
+					shBankRepository.getTransactionDetailById(transactionId))
+				.orElseThrow(() -> new ExceptionResponse(
+					CustomException.NOT_FOUND_TRANSACTION_DETAIL_EXCEPTION));
+		} catch (NonUniqueResultException e) {
+			throw new ExceptionResponse(CustomException.NOT_GET_TRANSACTION_DETAIL_EXCEPTION);
+		}
+	}
 
-    @Transactional
-    @Override
-    public void connectMyDataAccount(String tokenHeader) {
-        String phoneNumber = jwtUtil.getLoginUserPhoneNumber(tokenHeader);
-        shBankRepository.connectMyDataAccount(phoneNumber);
-    }
+	@Transactional
+	@Override
+	public void connectMyDataAccount(String tokenHeader) {
+		String phoneNumber = jwtUtil.getLoginUserPhoneNumber(tokenHeader);
+		shBankRepository.connectMyDataAccount(phoneNumber);
+	}
 
-    @Transactional
-    @Override
-    public BankTransferResponseDTO addFundsToAccount(AccountTransferRequestDTO requestDTO) {
-        ShBank shBank = shBankRepository.findByAccountNumber(requestDTO.depositAccountNumber())
-            .orElseThrow(() -> new ExceptionResponse(CustomException.ACCOUNT_NOT_FOUND_EXCEPTION));
+	@Transactional
+	@Override
+	public BankTransferResponseDTO addFundsToAccount(AccountTransferRequestDTO requestDTO) {
+		ShBank shBank = shBankRepository.findByAccountNumber(requestDTO.depositAccountNumber())
+			.orElseThrow(() -> new ExceptionResponse(CustomException.ACCOUNT_NOT_FOUND_EXCEPTION));
 
-        long accountTransactionBalance = shBank.getAccountBalance() + requestDTO.amount();
-        ShBankTransaction transaction = ShBankTransaction.createAccountTransferHistory(shBank,
-            requestDTO, accountTransactionBalance);
+		long accountTransactionBalance = shBank.getAccountBalance() + requestDTO.amount();
+		ShBankTransaction transaction = ShBankTransaction.createAccountTransferHistory(shBank,
+			requestDTO, accountTransactionBalance);
 
-        shBank.addTransaction(transaction);
+		shBank.addTransaction(transaction);
 
-        return BankTransferResponseDTO.fromBankTransferResponseDTO(shBank);
-    }
+		return BankTransferResponseDTO.fromBankTransferResponseDTO(shBank);
+	}
 
-    @Override
-    public MonthlyTransactionHistoryListDTO getMonthlyTransactionHistory(String tokenHeader,
-        MonthlyTransactionRequestDTO requestDTO) {
-        String phoneNumber = jwtUtil.getLoginUserPhoneNumber(tokenHeader);
-        MonthlyTransactionHistoryListDTO transactions = shBankRepository.getTransactionByPhoneNumberAndYearMonth(
-            phoneNumber, requestDTO);
+	@Override
+	public MonthlyTransactionHistoryListDTO getMonthlyTransactionHistory(String tokenHeader,
+		MonthlyTransactionRequestDTO requestDTO) {
+		String phoneNumber = jwtUtil.getLoginUserPhoneNumber(tokenHeader);
+		MonthlyTransactionHistoryListDTO transactions = shBankRepository.getTransactionByPhoneNumberAndYearMonth(
+			phoneNumber, requestDTO);
 
-        return transactions;
-    }
+		return transactions;
+	}
 
-    @Override
-    public Map<Category, Long> getTransactionByGroupCategory(
-        BankToAnalysisMessageRequestDTO requestDTO) {
-        return shBankRepository.getTransactionByGroupCategory(requestDTO);
-    }
+	@Override
+	public Map<Category, Long> getTransactionByGroupCategory(
+		BankToAnalysisMessageRequestDTO requestDTO) {
+		return shBankRepository.getTransactionByGroupCategory(requestDTO);
+	}
+
+	@Override
+	public SearchedTransactionResponseDTO searchTransaction(
+		final SearchTransactionRequestDTO searchTransactionRequestDTO) {
+		Pageable pageable = PageRequest.of(searchTransactionRequestDTO.page(),
+			searchTransactionRequestDTO.size());
+		Page<SearchedTransaction> searchedTransactions = getResult(searchTransactionRequestDTO,
+			pageable);
+
+		boolean isLastPage = searchedTransactions.isLast();
+
+		return SearchedTransactionResponseDTO.fromSearchedTransaction(
+			searchedTransactions.getContent(), isLastPage);
+	}
+
+	private Page<SearchedTransaction> getResult(
+		SearchTransactionRequestDTO searchTransactionRequestDTO, Pageable pageable) {
+		if (searchTransactionRequestDTO.deposit() != null) {
+			return searchedTransactionRepository.findByAccountIdAndDepositContaining(
+				searchTransactionRequestDTO.accountId(),
+				searchTransactionRequestDTO.deposit(), pageable);
+		}
+		return searchedTransactionRepository.findByAccountId(
+			searchTransactionRequestDTO.accountId(), pageable);
+	}
+
+	@Transactional
+	@Override
+	public PaymentResponseDTO qrPayment(final Long accountId,
+		final PaymentRequestDTO paymentRequestDTO) {
+		ShBank shBank = shBankRepository.findById(accountId)
+			.orElseThrow(() -> new ExceptionResponse(CustomException.ACCOUNT_NOT_FOUND_EXCEPTION));
+		long transactionBalance = calculateTransactionBalance(shBank,
+			paymentRequestDTO.transactionAmount());
+
+		ShBankTransaction paymentTransaction = createTransaction(shBank, paymentRequestDTO,
+			transactionBalance);
+
+		shBank.addTransaction(paymentTransaction);
+
+		shBankTransactionRepository.save(paymentTransaction);
+
+		searchedTransactionRepository.save(
+			SearchedTransaction.fromAccountIdAndKbBankTransaction(accountId, paymentTransaction));
+
+		return PaymentResponseDTO.fromPaymentResponseDTO(paymentTransaction);
+	}
+
+	private long calculateTransactionBalance(ShBank shBank, long transactionAmount) {
+		long transactionBalance = shBank.getAccountBalance() - transactionAmount;
+		if (transactionBalance < 0) {
+			throw new ExceptionResponse(CustomException.NOT_ENOUGH_FUNDS_EXCEPTION);
+		}
+		return transactionBalance;
+	}
+
+	private ShBankTransaction createTransaction(ShBank shBank,
+		PaymentRequestDTO paymentRequestDTO, long transactionBalance) {
+		return ShBankTransaction.builder()
+			.transactionAmount(-1 * paymentRequestDTO.transactionAmount())
+			.transactionDatetime(LocalDateTime.now())
+			.deposit(paymentRequestDTO.deposit())
+			.withdrawal(shBank.getAccountNumber())
+			.transactionBalance(transactionBalance)
+			.build();
+	}
 }
