1331572b52335c7a2547c9b55c52143f32d93107
HaJeongSu <htg770@gmail.com>
2024-10-05 18:22:50 +0900

    feat: 스크롤 진짜 수정..

--- FrontEnd/challet/dev-dist/sw.js
+++ FrontEnd/challet/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.t7dot7ijhho"
+    "revision": "0.afj72a9kvmo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
