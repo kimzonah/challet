cdcefc172d1da6d650f70d814d9ee0df7634d08f
kimseonha <ksh219805@gmail.com>
2024-10-07 02:05:25 +0900

    Revert "refactor: 검색 챌린지 타입 변경(record -> class)"
    This reverts commit 65b248b870de35bb37330e3032c0819fd9a3902e.

--- ChalletService/src/main/java/com/challet/challetservice/domain/entity/SearchedChallenge.java
+++ ChalletService/src/main/java/com/challet/challetservice/domain/entity/SearchedChallenge.java
@@ -2,10 +2,7 @@ package com.challet.challetservice.domain.entity;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
@@ -13,47 +10,46 @@ import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
 @Document(indexName = "challenges")
 @Schema(description = "챌린지 검색")
-public class SearchedChallenge {
+public record SearchedChallenge(
 
     @Id
     @Schema(description = "챌린지ID")
-    private String challengeId;
+    String challengeId,
 
     @Field(type = FieldType.Keyword)
     @Schema(description = "챌린지 진행 상태", allowableValues = {"RECRUITING", "PROGRESSING", "END"})
-    private String status;
+    String status,
 
     @Field(type = FieldType.Keyword)
     @Schema(description = "챌린지 카테고리", allowableValues = {"DELIVERY", "TRANSPORT", "COFFEE", "SHOPPING"})
-    private String category;
+    String category,
 
     @Field(type = FieldType.Text)
     @Schema(description = "챌린지 제목")
-    private String title;
+    String title,
 
     @Schema(description = "챌린지 소비한도")
-    private Long spendingLimit;
+    Long spendingLimit,
 
     @Field(type = FieldType.Date, format = DateFormat.date, pattern = "yyyy-MM-dd")
     @Schema(description = "챌린지 시작날짜")
-    private LocalDate startDate;
+    LocalDate startDate,
 
     @Field(type = FieldType.Date, format = DateFormat.date, pattern = "yyyy-MM-dd")
     @Schema(description = "챌린지 마감날짜")
-    private LocalDate endDate;
+    LocalDate endDate,
 
     @Schema(description = "참여 가능 인원")
-    private Integer maxParticipants;
+    Integer maxParticipants,
 
     @Schema(description = "현재 참여 인원")
-    private Integer currentParticipants;
+    Integer currentParticipants,
 
     @Schema(description = "공개 여부 (비공개 : false, 공개 : true)")
-    private Boolean isPublic;
+    Boolean isPublic
+) {
 
     public static SearchedChallenge fromChallenge(Challenge challenge) {
         return SearchedChallenge.builder()
