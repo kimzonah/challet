c5876ebc16f063410c4aad967b9906f56eb5d243
kkkhhh4930 <kkkhhh4930@naver.com>
2024-10-07 01:33:03 +0900

    feat: mypage nickname

--- FrontEnd/challet/dev-dist/sw.js
+++ FrontEnd/challet/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.r2hctpr91cg"
+    "revision": "0.u3l93k7i83g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
