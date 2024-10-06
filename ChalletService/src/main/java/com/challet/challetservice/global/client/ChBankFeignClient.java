058409c605d6101e48a656093792d39727d58b98
kimseonha <ksh219805@gmail.com>
2024-10-06 20:25:45 +0900

    feat: 비밀번호 암호화

--- ChalletService/src/main/java/com/challet/challetservice/global/client/ChBankFeignClient.java
+++ ChalletService/src/main/java/com/challet/challetservice/global/client/ChBankFeignClient.java
@@ -4,10 +4,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "ch-bank")
 public interface ChBankFeignClient {
 
     @PostMapping("/api/ch-bank")
-    ResponseEntity<String> requestCreateChBankAccount(@RequestHeader("phoneNumber") String phoneNumber);
+    ResponseEntity<String> requestCreateChBankAccount(@RequestParam("name") String name,
+        @RequestHeader("phoneNumber") String phoneNumber);
 }
