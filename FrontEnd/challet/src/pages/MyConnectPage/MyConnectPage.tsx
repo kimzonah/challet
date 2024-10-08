046245496396920940dea42f6298bd9b137c338c
YuJin Son <156387293+yjjj1612@users.noreply.github.com>
2024-10-09 00:51:19 +0900

    마이데이터 수정 페이지 구현

--- FrontEnd/challet/src/pages/MyConnectPage/MyConnectPage.tsx
+++ FrontEnd/challet/src/pages/MyConnectPage/MyConnectPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../../api/axiosInstance';
 import { TopBar } from '../../components/topbar/topbar';
