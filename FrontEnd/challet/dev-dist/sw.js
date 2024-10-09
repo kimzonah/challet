637e6c1a1dbcc668744bf50b75325ecd9082c229
kkkhhh4930 <kkkhhh4930@naver.com>
2024-10-10 03:26:30 +0900

    feat: 이거 안 되면 안 함

--- FrontEnd/challet/dev-dist/sw.js
+++ FrontEnd/challet/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.54gvv3lk2n8"
+    "revision": "0.04o95apjcp"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
