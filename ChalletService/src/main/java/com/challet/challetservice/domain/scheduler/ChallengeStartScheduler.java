40dde330f537a7cc39a88964a750e2458a50e0a7
kimseonha <ksh219805@gmail.com>
2024-10-14 22:26:17 +0900

    feat: 시작 2229

--- ChalletService/src/main/java/com/challet/challetservice/domain/scheduler/ChallengeStartScheduler.java
+++ ChalletService/src/main/java/com/challet/challetservice/domain/scheduler/ChallengeStartScheduler.java
@@ -20,7 +20,7 @@ public class ChallengeStartScheduler {
     private final ChallengeRepository challengeRepository;
     private final SearchedChallengeRepository searchedChallengeRepository;
 
-    @Scheduled(cron = "0 26 22 * * ?")
+    @Scheduled(cron = "0 29 22 * * ?")
     @Transactional
     public void startChallenge() {
 
