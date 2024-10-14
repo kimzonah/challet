045b223a990b17e42a0e80f25dd4f63ad29589b6
HaJeongSu <htg770@gmail.com>
2024-10-14 22:56:24 +0900

    feat: file2URL api로 수정

--- FrontEnd/challet/src/hooks/useFile2URL.tsx
+++ FrontEnd/challet/src/hooks/useFile2URL.tsx
@@ -1,36 +1,13 @@
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
-import { v4 as uuidv4 } from 'uuid'; // uuid import
-
 const useFile2URL = () => {
-  const region = 'ap-northeast-2';
-  const bucket = 'challetbucket';
-
-  const s3 = new S3Client({
-    region: region,
-    credentials: {
-      accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
-      secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
-    },
-  });
-
   const file2URL = async (file: File) => {
-    // 고유한 UUID를 생성하고 파일의 확장자를 유지
-    const uniqueId = uuidv4(); // 고유 ID 생성
-    const fileExtension = file.name.split('.').pop(); // 파일 확장자 추출
-    const uniqueFileName = `${uniqueId}.${fileExtension}`; // 고유 파일명 생성
-
-    const params = {
-      Bucket: bucket,
-      Key: uniqueFileName, // 고유한 파일 이름 사용
-      Body: file,
-    };
-
     try {
-      const command = new PutObjectCommand(params);
-      const data = await s3.send(command);
-      const fileURL = `https://${bucket}.s3.${region}.amazonaws.com/${uniqueFileName}`;
-      console.log(data);
-      // console.log('File URL:', fileURL);
+      const api = import.meta.env.VITE_API_URL + '/api/challet/upload';
+      const formData = new FormData();
+      formData.append('file', file);
+      const fileURL = await fetch(api, {
+        method: 'POST',
+        body: formData,
+      }).then((res) => res.text());
       return fileURL;
     } catch (error) {
       console.error('Error uploading file:', error);
