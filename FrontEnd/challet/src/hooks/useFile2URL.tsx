e771b72f18fe52dd1f73544a675b145ca8b12d80
HaJeongSu <htg770@gmail.com>
2024-10-15 00:38:40 +0900

    feat: 이미지 등록 수정

--- FrontEnd/challet/src/hooks/useFile2URL.tsx
+++ FrontEnd/challet/src/hooks/useFile2URL.tsx
@@ -1,16 +1,17 @@
-import accessToken from '../store/useAuthStore';
+import useAuthStore from '../store/useAuthStore';
 
 const useFile2URL = () => {
   const file2URL = async (file: File) => {
     try {
       const api = import.meta.env.VITE_API_URL + '/api/challet/upload';
+      const token = useAuthStore.getState().accessToken;
       const formData = new FormData();
       formData.append('file', file);
       const fileURL = await fetch(api, {
         method: 'POST',
         body: formData,
         headers: {
-          Authorization: `Bearer ${accessToken}`, // 필요한 경우 토큰을 헤더에 추가
+          Authorization: `Bearer ${token}`, // 필요한 경우 토큰을 헤더에 추가
         },
       }).then((res) => res.text());
       console.log('Uploaded file URL:', fileURL);
